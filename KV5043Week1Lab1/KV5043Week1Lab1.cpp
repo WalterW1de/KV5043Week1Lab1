da638238b1b4a0461fdd12baee24f0659b22a9ff
ChrisT0403 <christroughton34@gmail.com>
2024-10-10 22:25:00 +0100

    Traditional for loops vs Range based for loops

--- KV5043Week1Lab1/KV5043Week1Lab1.cpp
+++ KV5043Week1Lab1/KV5043Week1Lab1.cpp
@@ -3,10 +3,28 @@
 int main()
 {
 	int enemyHealth[5] = { 100, 90, 80, 70, 60 };
-	float enemySpeeds[4] = { 1.5f, 2.3567f, 0.245f, 0.9999999f };
-	double enemyXPositions[6] = { 10.75, 2.3567, 0.245, 0.9999999, 9.9999999999, 1.0 };
+	float enemySpeeds[4] = { 1.5f, 2.356f, 0.245f, 0.9999999f };
+	double enemyXPositions[4] = { 2.3567, 0.245, 0.9999999, 9.9999999999};
 	char playerName[5] = { 'C', 'h', 'r', 'i', 's' };
 
+	std::cout << "Enemy Health (traditional for loop): " << std::endl;
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << enemyHealth[i] << std::endl;
+	}
+
+	std::cout << "Enemy Speeds (traditional for loop): " << std::endl;
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << enemySpeeds[i] << std::endl;
+	}
+
+	int i = 0;
+	for (double Positions : enemyXPositions)
+	{
+		std::cout << "Enemy" << i + 1 << "Position" << Positions << std::endl;
+		i++;
+	}
 
 	return 0;
 }
