a3af322bf6262a76f4d59dccb70f4a50ef4e3b4c
ChrisT0403 <christroughton34@gmail.com>
2024-10-10 21:42:01 +0100

    First C++ Variables

--- KV5043Week1Lab1/KV5043Week1Lab1.cpp
+++ KV5043Week1Lab1/KV5043Week1Lab1.cpp
@@ -5,16 +5,16 @@
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+	int playerHealth = 100;
+	float enemySpeed = 1.5;
+	double playerXPosition = 10.75;
+	char playerInitial = 'C';
+	bool isGameOver = false;
+
+	std::cout << "Player health (int):" << playerHealth << std::endl;
+	std::cout << "Enemy Speed (Float):" << enemySpeed<< std::endl;
+	std::cout << "Player X Position (double):" << playerXPosition << std::endl;
+	std::cout << "Player Initial (Character):" << playerInitial << std::endl;
+	std::cout << "Is Game Over (bool):" << isGameOver << std::endl;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
