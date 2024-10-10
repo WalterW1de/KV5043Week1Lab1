e93e89917452cfd032e15d8048c776bae7c4b32a
ChrisT0403 <christroughton34@gmail.com>
2024-10-10 22:55:15 +0100

    Type Casting in C++

--- KV5043Week1Lab1/KV5043Week1Lab1.cpp
+++ KV5043Week1Lab1/KV5043Week1Lab1.cpp
@@ -2,10 +2,28 @@
 
 int main()
 {
-	char playerFirstName[6] = { 'C', 'h', 'r', 'i', 's', 0};
-	char playerSecondName[10] = { 'T', 'r', 'o', 'u', 'g', 'h', 't', 'o', 'n', 0 };
+	int enemyYPosition = 100;
+	float enemyYVelocity = 2.3f;
 
-	std::cout << playerFirstName << " " << playerSecondName << std::endl;
+int enemyYvelocityInt = (int)enemyYVelocity;
+
+	std::cout << enemyYPosition << std::endl;
+
+	enemyYPosition = enemyYPosition * enemyYVelocity; // Implicit Cast of enemyYVelocity from float to int
+
+	std::cout << enemyYPosition << std::endl;
+
+	enemyYPosition = (float)enemyYPosition * enemyYVelocity; // Explicit Cast of enemyYVelocity from float to int
+	
+	std::cout << enemyYPosition << std::endl;
+
+	enemyYPosition = float(enemyYPosition) * enemyYVelocity; // Explicit Functional Cast of enemyYVelocity from float to int
+
+	std::cout << enemyYPosition << std::endl;
+
+	enemyYPosition = static_cast <float>(enemyYPosition * enemyYVelocity); // Explicit C++ style Cast of enemyYVelocity from float to int
+
+	std::cout << enemyYPosition << std::endl;
 
 		return 0;
 }
