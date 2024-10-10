7c84ef95b9ee6859494b1549026c1356d66d6419
ChrisT0403 <christroughton34@gmail.com>
2024-10-10 22:34:09 +0100

    Character arrays vs C-style strings

--- KV5043Week1Lab1/KV5043Week1Lab1.cpp
+++ KV5043Week1Lab1/KV5043Week1Lab1.cpp
@@ -2,29 +2,10 @@
 
 int main()
 {
-	int enemyHealth[5] = { 100, 90, 80, 70, 60 };
-	float enemySpeeds[4] = { 1.5f, 2.356f, 0.245f, 0.9999999f };
-	double enemyXPositions[4] = { 2.3567, 0.245, 0.9999999, 9.9999999999};
-	char playerName[5] = { 'C', 'h', 'r', 'i', 's' };
+	char playerFirstName[6] = { 'C', 'h', 'r', 'i', 's', 0};
+	char playerSecondName[10] = { 'T', 'r', 'o', 'u', 'g', 'h', 't', 'o', 'n', 0 };
 
-	std::cout << "Enemy Health (traditional for loop): " << std::endl;
-	for (int i = 0; i < 5; i++)
-	{
-		std::cout << enemyHealth[i] << std::endl;
-	}
+	std::cout << playerFirstName << " " << playerSecondName << std::endl;
 
-	std::cout << "Enemy Speeds (traditional for loop): " << std::endl;
-	for (int i = 0; i < 4; i++)
-	{
-		std::cout << enemySpeeds[i] << std::endl;
-	}
-
-	int i = 0;
-	for (double Positions : enemyXPositions)
-	{
-		std::cout << "Enemy" << i + 1 << "Position" << Positions << std::endl;
-		i++;
-	}
-
-	return 0;
+		return 0;
 }
